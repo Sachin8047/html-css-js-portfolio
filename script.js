f3b7bdc5e6ad1a636214a6688b2de4fddcf1227f
Sachin Lamoria <sachinlamoria111@gmail.com>
2025-01-10 21:56:43 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+function togglemenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".humburger-icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+function myexperience() {
+  location.href = "#experience";
+}
